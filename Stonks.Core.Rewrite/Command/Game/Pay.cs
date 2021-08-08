e6140b8fff5049b80ad566de812aed0105964b98
Kate Lin <juni070127@gmail.com>
2021-08-08 14:22:57 +0900

    Cooldown 애트리뷰트 만들었음

---
+++ Stonks.Core.Rewrite/Command/Game/Pay.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using Stonks.Core.Rewrite.Class;
+using System.Threading.Tasks;
+
+namespace Stonks.Core.Rewrite.Command.Game
+{
+    public class Pay : ModuleBase<SocketCommandContext>
+    {
+        [Command("용돈")]
+        [Cooldown(60)]
+        public async Task HelpAsync()
+        {
+            await ReplyAsync("Test");
+        }
+    }
+}
