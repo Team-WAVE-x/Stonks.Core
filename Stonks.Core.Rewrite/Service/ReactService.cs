c23266443c7cac24e3acca133927fd57e2582dc9
Kate Lin <juni070127@gmail.com>
2021-08-17 14:47:53 +0900

    파업합미다.

--- Stonks.Core.Rewrite/Service/ReactService.cs
+++ Stonks.Core.Rewrite/Service/ReactService.cs
@@ -2,6 +2,7 @@
 using Discord.Rest;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using Stonks.Core.Rewrite.Class;
 using Stonks.Core.Rewrite.Extension;
 using System;
 using System.Collections.Generic;
@@ -26,12 +27,20 @@ namespace Stonks.Core.Rewrite.Service
 
         public static void AddReactionMessage(RestUserMessage message, ulong userId, List<Dictionary<IEmote, Action>> dictionaries, TimeSpan timeout, bool removeMessageAfterTimeOut = false)
         {
-            ObjectCache cache = MemoryCache.Default;
+            var cache = MemoryCache.Default;
+            var policy = new CacheItemPolicy
+            {
+                SlidingExpiration = timeout
+            };
+            var reactMessage = new ReactMessage(message, userId, dictionaries, removeMessageAfterTimeOut);
+
+            cache.Add(message.Id.ToString(), reactMessage, policy);
         }
 
         public static void RemoveReactionMessage(ulong messageId)
         {
-            ObjectCache cache = MemoryCache.Default;
+            var cache = MemoryCache.Default;
+            cache.Remove(messageId.ToString());
         }
 
         private void CacheRemovedCallback(CacheEntryRemovedArguments arguments)
