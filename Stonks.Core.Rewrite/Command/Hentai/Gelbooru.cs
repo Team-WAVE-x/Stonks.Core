09e15c9c3eb8ece6a7a559ffced3011c20702c07
Kate Lin <juni070127@gmail.com>
2021-08-28 13:25:38 +0900

    이건 미친짓이야, 난 여기서 나가야겠

--- Stonks.Core.Rewrite/Command/Hentai/Gelbooru.cs
+++ Stonks.Core.Rewrite/Command/Hentai/Gelbooru.cs
@@ -1,20 +1,22 @@
 ﻿using Discord;
 using Discord.Commands;
 using Interactivity;
+using Stonks.Core.Rewrite.Precondition;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stonks.Core.Rewrite.Command.Nsfw
+namespace Stonks.Core.Rewrite.Command.Hentai
 {
     public class Gelbooru : ModuleBase<SocketCommandContext>
     {
+        [Nsfw]
         [Command("겔부루")]
         public async Task GelbooruAsync([Remainder] string tags = null)
         {
-
+            
         }
     }
 }
---
+++ Stonks.Core.Rewrite/Precondition/Nsfw.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stonks.Core.Rewrite.Precondition
+{
+    public class Nsfw : PreconditionAttribute
+    {
+        private readonly string _errorMessage;
+
+        public Nsfw(string errorMessage = "❌ 본 명령어는 NSFW 채널에서만 사용할 수 있습니다.")
+        {
+            _errorMessage = errorMessage;
+        }
+
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var channel = context.Channel as ITextChannel;
+
+            if (channel != null && channel.IsNsfw)
+            {
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            }
+            else
+            {
+                context.Channel.SendMessageAsync(_errorMessage);
+                return Task.FromResult(PreconditionResult.FromError(_errorMessage));
+            }
+        }
+    }
+}
